2f04174e86192147e0987517ca5476ee148fb85d
장영재 <urinaner@naver.com>
2024-11-04 20:31:53 +0900

    chore: 기본 세팅

---
+++ src/main/java/org/yj/sejongauth/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package org.yj.sejongauth.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.yj.sejongauth")
+public class AutoConfig {
+}
