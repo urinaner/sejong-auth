7a586e4fffc72e97b10e8e8c0bddf6bed88b74d9
장영재 <urinaner@naver.com>
2024-11-04 19:57:36 +0900

    init: 초기 세팅

---
+++ src/test/java/org/yj/sejongauth/SejongAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yj.sejongauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejongAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
