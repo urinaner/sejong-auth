ab1e2790f350558c702cc54c589302cade19ab8f
장영재 <urinaner@naver.com>
2024-11-05 16:53:38 +0900

    feat: ProfileRes

---
+++ src/main/java/org/yj/sejongauth/domain/ProfileRes.java
@@ -0,0 +1,31 @@
+package org.yj.sejongauth.domain;
+
+public class ProfileRes {
+    private final String major;
+    private final String studentCode;
+    private final String name;
+    private final int gradeLevel;
+    private final String userStatus;
+    private final int completedSemesters;
+    private final int verifiedSemesters;
+
+    public ProfileRes(String major, String studentCode, String name,
+                      int gradeLevel, String userStatus,
+                      int completedSemesters, int verifiedSemesters) {
+        this.major = major;
+        this.studentCode = studentCode;
+        this.name = name;
+        this.gradeLevel = gradeLevel;
+        this.userStatus = userStatus;
+        this.completedSemesters = completedSemesters;
+        this.verifiedSemesters = verifiedSemesters;
+    }
+
+    public String getMajor() { return major; }
+    public String getStudentCode() { return studentCode; }
+    public String getName() { return name; }
+    public int getGradeLevel() { return gradeLevel; }
+    public String getUserStatus() { return userStatus; }
+    public int getCompletedSemesters() { return completedSemesters; }
+    public int getVerifiedSemesters() { return verifiedSemesters; }
+}
