077669c992f8391da5d7a3a4022ad7c2434d57b6
장영재 <urinaner@naver.com>
2024-11-06 15:21:11 +0900

    refactor: 설정 변경

--- src/main/java/org/yj/sejongauth/config/AutoConfig.java
+++
@@ -1,9 +0,0 @@
-package org.yj.sejongauth.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("org.yj.sejongauth")
-public class AutoConfig {
-}
